971e1f3103f4fdc2501500ce5289adf380d39fe8
John Siddoway <john.pez.s@gmail.com>
2020-11-17 08:39:04 -0800

    Commit the latest set of changes. Pages seem good out to page 8.

--- page.js
+++ page.js
@@ -5,7 +5,15 @@ function Page() {
     self.monstersSelected = ko.observableArray();
 
     self.selectMonster = function(monster) {
-        self.monstersSelected.push(monster);
+        var clone = Object.assign({}, monster);
+        // By setting an id field on the clone to something mostly random, it'll let me remove them one at a time.
+        // By not setting this on the input monster, I can treat each clone as (hopefully) truly unique.
+        clone.id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        self.monstersSelected.push(clone);
+    }
+
+    self.removeMonster = function(monster) {
+        self.monstersSelected.remove(monster);
     }
 
     self.init = function (monsters) {
