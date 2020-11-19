9481ad60ba80074a74cb992e9426b56ce21ef616
John Siddoway <john.pez.s@gmail.com>
2020-11-18 16:08:25 -0800

    Force the creation of a new 'page' if we think the new statblock will push us onto a new page.

--- page.js
+++ page.js
@@ -1,9 +1,35 @@
+function monsterPage() {
+    var self = this;
+    self.pageSizes = new Map([
+        ["small", 1],
+        ["medium", 2],
+        ["large", 4]
+    ]);
+    self.maxSize = 4;
+    self.monsters = ko.observableArray();
+    self.currentSize = ko.computed(function(){
+        var total = 0;
+        for(var p = 0; p < self.monsters().length; ++p)
+        {
+            total += self.pageSizes.get(self.monsters()[p].pageSize);
+        }
+        return total;
+    });
+    self.canAdd = function(monster) {
+        return self.currentSize() + self.pageSizes.get(monster.pageSize) <= self.maxSize;
+    }
+    self.addMonster = function(monster) {
+        self.monsters.push(monster);
+    }
+};
+
 function Page() {
     var self = this;
     self.abilityScoreModifiers = [0, -5, -4, -4, -3, -3, -2, -2, -1, -1, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10];
     self.crToXP = new Map();
     self.monsters = ko.observableArray();
     self.monstersSelected = ko.observableArray();
+    self.pages = ko.observableArray();
 
     self.selectMonster = function(monster) {
         var clone = Object.assign({}, monster);
@@ -11,6 +37,14 @@ function Page() {
         // By not setting this on the input monster, I can treat each clone as (hopefully) truly unique.
         clone.id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
         self.monstersSelected.push(clone);
+        var last = self.pages()[self.pages().length - 1];
+        if (last !== undefined && last.canAdd(clone)) {
+            last.addMonster(clone);
+        } else {
+            last = new monsterPage();
+            last.addMonster(clone);
+            self.pages.push(last);
+        }
     }
 
     self.removeMonster = function(monster) {
@@ -57,4 +91,4 @@ function Page() {
             [30, 155000]
         ]);
 	};
-}
+};
